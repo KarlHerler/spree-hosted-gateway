1c52482ffc129d95c5576edaa59a0390c788d9b8
Karl Herler <hi@karlherler.com>
2011-12-04 14:52:35 +0200

    Switche to stripping scands using the logic Ö->O

--- lib/external_gateway.rb
+++ lib/external_gateway.rb
@@ -31,8 +31,8 @@ class ExternalGateway < PaymentMethod
   def with_fire(s, param=:named)
     coder = HTMLEntities.new
     s = coder.encode(s)
-    s =  s.gsub(/å/, "&aring;").gsub(/ä/, '&auml;').gsub(/ö/, "&ouml;") if s.class()=="asd".class()
-    return s.gsub(/Å/, "&Aring;").gsub(/Ä/, '&Auml;').gsub(/Ö/, "&Ouml;") if s.class()=="asd".class()
+    s =  s.gsub(/å/, "a").gsub(/ä/, 'a').gsub(/ö/, "o") if s.class()=="asd".class()
+    return s.gsub(/Å/, "A").gsub(/Ä/, 'A').gsub(/Ö/, "O") if s.class()=="asd".class()
     #return s.encode('UTF-8')
   end
   def num_to_s(param)
